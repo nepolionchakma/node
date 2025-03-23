2e39c2239c356196b6c2a0486260818bc8becb81
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-03-23 17:57:35 +0600

    test/server API end point added for test API

--- Routes/authenticationRoutes.js
+++ Routes/authenticationRoutes.js
@@ -3,6 +3,9 @@ const authentication = require("../Authentication/Authentication");
 
 const router = Router();
 
+router.get("/server", (req, res) => {
+  res.send("Server is running");
+});
 router.post("/", authentication.login);
 router.get("/", authentication.logout);
 router.get("/user", authentication.user);
