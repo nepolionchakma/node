4adb0454ca12912366e46d8765209f0a852b2c6c
Jyanial Dewan <djyanial@gmail.com>
2025-04-09 15:31:13 +0600

    mobile menu

---
+++ Controller/mobileMenuController.js
@@ -0,0 +1,74 @@
+const prisma = require("../DB/db.config");
+
+exports.getMobileMenu = async (req, res) => {
+  try {
+    const mobileMenu = await prisma.mobile_menu.findMany();
+
+    return res.status(200).json(mobileMenu);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.createMobileMenu = async (req, res) => {
+  try {
+    const { menu_code, menu_name, menu_desc, menu_structure } = req.body;
+    const mobileMenu = await prisma.mobile_menu.findMany();
+    const maxID =
+      mobileMenu.length > 0
+        ? Math.max(...mobileMenu.map((item) => item.menu_id)) + 1
+        : 1;
+
+    if (!menu_code || !menu_name || !menu_desc || !menu_structure) {
+      return res.status(422).json({
+        message: "menu_code, menu_name, menu_desc, menu_structure is required",
+      });
+    }
+
+    const newMobileMenu = await prisma.mobile_menu.create({
+      data: {
+        menu_id: maxID,
+        menu_code: menu_code,
+        menu_name: menu_name,
+        menu_desc: menu_desc,
+        menu_structure: menu_structure,
+      },
+    });
+
+    return res.status(201).json(newMobileMenu);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.updateMobileMenu = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const { menu_code, menu_name, menu_desc, menu_structure } = req.body;
+    const mobileMenu = await prisma.mobile_menu.findUnique({
+      where: {
+        menu_id: id,
+      },
+    });
+
+    if (!mobileMenu) {
+      return res.status(404).json({ message: "Mobile Menu not found" });
+    }
+
+    const updatedMobileMenu = await prisma.mobile_menu.update({
+      where: {
+        menu_id: id,
+      },
+      data: {
+        menu_code: menu_code,
+        menu_name: menu_name,
+        menu_desc: menu_desc,
+        menu_structure: menu_structure,
+      },
+    });
+
+    return res.status(201).json(updatedMobileMenu);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
