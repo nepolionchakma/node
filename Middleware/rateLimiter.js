f9a69c7d43fa7634e29fee883dc909edebaeb4ea
Jyanial Dewan <djyanial@gmail.com>
2025-11-24 12:22:36 +0600

    rate limit 5 times

--- Middleware/rateLimiter.js
+++ Middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 exports.loginLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
-  max: 3, // 5 login attempts per window per IP
+  max: 5, // 5 login attempts per window per IP
   message: {
     message: "Too many attempts. Please try again after 60 minutes.",
   },
