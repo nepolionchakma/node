62a597167e0a983b22c6cd0ed7a49fc868038a77
Jyanial Dewan <djyanial@gmail.com>
2025-11-24 12:21:59 +0600

    rate-limit

--- Routes/authenticationRoutes.js
+++ Routes/authenticationRoutes.js
@@ -1,9 +1,10 @@
 const Router = require("express");
 const authentication = require("../Authentication/Authentication");
+const { loginLimiter } = require("../Middleware/rateLimiter");
 
 const router = Router();
 
-router.post("/", authentication.login);
+router.post("/", loginLimiter, authentication.login);
 router.get("/", authentication.logout);
 router.get("/user", authentication.user);
 router.get("/refresh-token", authentication.refreshToken);
