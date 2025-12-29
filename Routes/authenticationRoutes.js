30eada85cec19af4763f93c75d1d3d3044f8ce56
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-12-29 13:01:11 +0600

    try another way feature apis added

--- Routes/authenticationRoutes.js
+++ Routes/authenticationRoutes.js
@@ -11,5 +11,7 @@ router.get("/refresh-token", authentication.refreshToken);
 // router.post("/", authentication.verifyToken);
 router.post("/verify-token", authentication.verifyToken);
 router.post("/verify-mfa-login", authentication.verifyMFALogin);
+router.post("/send-email-mfa-code", authentication.sendEmailMfaCode);
+router.post("/verify-email-mfa-code", authentication.verifyEmailedMfaCode);
 
 module.exports = router;
